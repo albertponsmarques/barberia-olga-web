2fabde7c29e7a99f981c6a52d3a94b8320deaa9c
albert <albert>
2022-07-13 12:09:16 +0200

    initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { withRouter } from "react-router";
+
+const Home = () => {
+  return (
+    <div
+      id="home">
+      <div id="homeContent">
+        <div className="page-title">Home Page</div>
+        <h3> Welcome! </h3>
+      </div>
+    </div>
+  );
+};
+
+export default (Home);
