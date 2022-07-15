48b0111aa6a6a22671cc6a694c1c2eeb82d81f2f
albert <albert>
2022-07-15 13:11:39 +0200

    siu

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,14 @@ import { Popover, Transition } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import Logo from "../resources/barber.svg";
 import Imagen from "../resources/imagenhome.jpg";
+import { AiFillHeart } from "react-icons/ai";
+
+//https://github.com/simonneutert/google-maps-reviews
 
 const navigation = [
   { name: "Inicio", href: "#" },
   { name: "Quienes somos", href: "#" },
-  { name: "Donde estamos", href: "#" },
+  { name: "Dónde estamos", href: "#" },
 ];
 
 export default function Home() {
@@ -121,9 +124,11 @@ export default function Home() {
           </Popover>
 
           <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-            <div className="sm:text-center lg:text-left">
+            <div className="sm:text-center lg:text-left mb-10">
               <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                <span className="inline-block text-brb-red xl:inline">Barberia</span>{" "}
+                <span className="inline-block text-brb-red xl:inline">
+                  Barberia
+                </span>{" "}
                 <span className="inline text-brb-blue xl:inline">
                   <img
                     alt="Barberia Olga"
@@ -137,11 +142,13 @@ export default function Home() {
                 Una peluquería o barbería es un local donde se ofrecen varios
                 servicios estéticos, principalmente el corte de pelo, pero
                 también suelen realizarse otros como afeitado, depilado,
-                manicura, pedicura, etc. Cuando se trata de muchos servicios
-                diferentes suele llamarse salón de belleza. La referencia más
-                antigua que se tiene sobre la existencia de cuidados cosméticos
-                en el pelo nos remite a Egipto, donde se empezaron a realizar
-                los cambios más significativos en cuanto a la cosmética capilar.
+                manicura, pedicura, etc. <br />
+                <br />
+                Cuando se trata de muchos servicios diferentes suele llamarse
+                salón de belleza. La referencia más antigua que se tiene sobre
+                la existencia de cuidados cosméticos en el pelo nos remite a
+                Egipto, donde se empezaron a realizar los cambios más
+                significativos en cuanto a la cosmética capilar.
               </p>
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
@@ -162,9 +169,18 @@ export default function Home() {
                 </div>
               </div>
             </div>
+            <div className="mt-36 content-center">
+              <p className="text-sm  text-gray-500 sm:mt-5 sm:text-sm sm:max-w-xl sm:mx-auto md:mt-5 md:text-sm lg:mx-0">
+                Opiniones
+              </p>{" "}
+              <p className="inline mt-3 text-xs text-gray-500 sm:mt-5 sm:text-xs sm:max-w-xl sm:mx-auto md:mt-5 md:text-xs lg:mx-0">
+                Porque nos importa tu opinion! 
+              </p>
+            </div>
           </main>
         </div>
       </div>
+
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <img
           className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
