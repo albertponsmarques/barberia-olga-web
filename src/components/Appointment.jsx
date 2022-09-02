571773b9c88504d4f9b4285e5997303aef04229b
albert <albert>
2022-09-02 12:58:03 +0200

    faltes

--- src/components/Appointment.jsx
+++ src/components/Appointment.jsx
@@ -8,15 +8,17 @@ export default function Appointment() {
       <Header />
       <div className="text-white grid h-10 place-items-center">
         <h1 className="mb-10 text-3xl">
-          Pide cita a traves de nuestro instagram!
+          Pide cita a través de nuestro instagram!
         </h1>
         <a href="https://www.instagram.com/barberiaolga/" target="_blank">
           <img src={Insta} className="h-24" />
         </a>
-        <h1 className="mt-24 text-3xl">O llamanos!</h1>
+        <p className="mt-4 text-xs">haz click en el icono</p>
+        <h1 className="mt-24 text-3xl">O llámanos!</h1>
         <a className="mt-10 text-4xl font-bold" href="tel:+34971381930">
           ☎ 971 38 19 30
         </a>
+        <p className="mt-4 text-xs">haz click para llamar</p>
       </div>
     </div>
   );
